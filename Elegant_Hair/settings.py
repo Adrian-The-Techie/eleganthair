71c8f02dd8242e0a974d9946f886f2afc10cad53
Adrian-The-Techie <adrianmuthomie@gmail.com>
2021-01-10 10:09:53 +0300

    First Deployment

--- Elegant_Hair/settings.py
+++ Elegant_Hair/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'ChicFavs/static/ChicFavs/')
 
 MEDIA_URL = "/media/"
