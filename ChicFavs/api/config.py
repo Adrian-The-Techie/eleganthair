94197cd00381eaee1e88d42fda98994ed38d77bb
Adrian-The-Techie <adrianmuthomie@gmail.com>
2021-01-10 12:49:01 +0300

    Turned debugging on

--- ChicFavs/api/config.py
+++ ChicFavs/api/config.py
@@ -1,3 +1,3 @@
-USERNAME="sandbox"
-API_KEY="0f0c36ede8e9e06c1e1bf8b61d67086bb6379d7976045a86ba5820495ed6a3d2"
-SMS_API="https://api.sandbox.africastalking.com/version1/messaging"
+USERNAME = "Chic-Favs-Admin"
+API_KEY = "0f0c36ede8e9e06c1e1bf8b61d67086bb6379d7976045a86ba5820495ed6a3d2"
+SMS_API = "https://api.africastalking.com/version1/messaging"
